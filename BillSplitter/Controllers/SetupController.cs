1ffa98e93e31b7424b287ac4ac7d5693ff2d69cd
Michael Coxon <michaelcoxon@outlook.com.au>
2018-06-25 19:47:29 +0800

    add setup controller

---
+++ BillSplitter/Controllers/SetupController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BillSplitter.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BillSplitter.Controllers
+{
+    [Route("api/[controller]")]
+    public class SetupController : Controller
+    {
+        private DbContext _dbContext;
+
+        public SetupController(BillSplitterContext dbContext)
+        {
+            this._dbContext = dbContext;
+        }
+
+        [ResponseCache(Duration = 0, NoStore = true)]
+        [HttpGet("run-migrations")]
+        public async Task<IActionResult> RunMigrationsAsync()
+        {
+            var migrator = this._dbContext.GetService<IMigrator>();
+            await migrator.MigrateAsync();
+            return Ok();
+        }
+    }
+}
